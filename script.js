ed3fd1522fda2add2ea1f2741b1bb48aaff715eb
smorty <smortensen@outlook.com>
2014-06-03 20:18:24 -0600

    creates a newfeed app that pulls from the Parse api

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-
+	$.ajax({
+		url: "https://api.parse.com/1/classes/NewsItem1",
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader('X-Parse-Application-Id', 'gzdH13KmNJDYnb7xNl3ZFRBnx3Hm3QVJXKpHoViW');
+			xhr.setRequestHeader('X-Parse-REST-API-Key', 'REoadyfaQ5OVoWyDn8O9p8Xwx5Wm807eqeUwrJdK');
+			}
+		})
+	.done(function(response) {
+		for(var i = 0; i < response.results.length; i++){
+			$('.newsfeed').append('<h2>'+response.results[i].title+'</h2>');
+			$('.newsfeed').append('<p>'+response.results[i].body+'</p>');
+		};
+	});
 });
